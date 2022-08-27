4ebd9b0df251c86ed5fcefcfc2e5c2bc92d6d599
Chanuth10 <it20074418@my.sliit.lk>
2022-08-27 16:58:05 +0530

    Implement update product backend api

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -2,12 +2,11 @@ const Product = require("../models/ProductModel.js");
 
 // create Product
 exports.createProduct = async (req, res, next) => {
-
   const product = await Product.create(req.body);
 
   res.status(201).json({
     success: true,
-    product
+    product,
   });
 };
 
@@ -18,5 +17,26 @@ exports.getAllProducts = async (req, res, next) => {
   res.status(200).json({
     success: true,
     products,
+  });
+};
+
+// Update Product ---Admin
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product is not found with this id",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useUnified: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
   });
 };
--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getAllProducts,
   createProduct,
+  updateProduct,
 } = require("../controller/ProductController");
 
 router.route("/products").get(getAllProducts);
 router.route("/products/create").post(createProduct);
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
