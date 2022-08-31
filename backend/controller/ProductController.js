6ae82df1c8906776272fa2ae690e4778a64297d4
Chanuth10 <it20074418@my.sliit.lk>
2022-08-31 08:42:48 +0530

    Filter some specific product set

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -16,7 +16,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 // get All Products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   
-  const feature = new Features(Product.find(), req.query).search();
+  const feature = new Features(Product.find(), req.query).search().filter();
   const products = await feature.query;
   res.status(200).json({
     success: true,
