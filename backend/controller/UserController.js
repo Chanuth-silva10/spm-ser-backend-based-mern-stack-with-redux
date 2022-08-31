59b2d1b11a47fda5965fd651b74a49868f2d1b17
Chanuth10 <it20074418@my.sliit.lk>
2022-08-31 09:26:49 +0530

    Implement user login route api

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,6 +1,7 @@
 const User = require("../models/UserModel");
 const ErrorHandler = require("../utils/ErrorHandler.js");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const { use } = require("../routes/UserRoute");
 
 // Register user
 exports.createUser = catchAsyncErrors(async (req, res, next) => {
@@ -12,14 +13,48 @@ exports.createUser = catchAsyncErrors(async (req, res, next) => {
     password,
     avatar: {
       public_id: "Profile",
-      url: "https://www.pexels.com/search/profile%20picture/"
-    }
+      url: "https://www.pexels.com/search/profile%20picture/",
+    },
   });
 
   const token = user.getJwtToken();
   console.log(token);
   res.status(201).json({
     success: true,
-    token
+    token,
   });
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter the email & password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+ 
+
+  if (!user) {
+    return next(
+      new ErrorHandler("User is not find with this email & password", 401)
+    );
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(
+      new ErrorHandler("User is not find with this email & password", 401)
+    );
+  }
+
+  const token = user.getJwtToken();
+
+  console.log(token);
+  res.status(201).json({
+    success: true,
+    token,
+  })
+
 });
