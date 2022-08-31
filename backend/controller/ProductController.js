41e39ad68584d7b36463b80245efb705e4d0bd31
Chanuth10 <it20074418@my.sliit.lk>
2022-08-31 08:35:27 +0530

    Implement and Control error handler and validation

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -1,33 +1,33 @@
 const Product = require("../models/ProductModel.js");
+const ErrorHandler = require("../utils/ErrorHandler.js");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
 
 // create Product
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 
 // Get All Product 
-exports.getAllProducts = async (req, res, next) => {
+exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
   const products = await Product.find();
 
   res.status(200).json({
     success: true,
     products,
   });
-};
+});
 
-// Update Product 
-exports.updateProduct = async (req, res, next) => {
+// Update Product
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product is not found with this id",
-    });
+    return next(new ErrorHandler("Product is not found with this id", 404));
   }
 
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -39,18 +39,16 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 // delete Product
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product is not found with this id",
-    });
+    return next(new ErrorHandler("Product is not found with this id", 404));
   }
+
 
   await product.remove();
 
@@ -58,19 +56,16 @@ exports.deleteProduct = async (req, res, next) => {
     success: true,
     message: "Product deleted succesfully",
   });
-};
+});
 
 // single Product details
-exports.getSingleProduct = async (req, res, next) => {
+exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product is not found with this id",
-    });
+    return next(new ErrorHandler("Product is not found with this id", 404));
   }
   res.status(200).json({
     success: true,
     product,
   });
-};
+});
--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -10,6 +10,10 @@ const {
 
 router.route("/products").get(getAllProducts);
 router.route("/products/create").post(createProduct);
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getSingleProduct);
+router
+  .route("/product/:id")
+  .put(updateProduct)
+  .delete(deleteProduct)
+  .get(getSingleProduct);
 
 module.exports = router;
