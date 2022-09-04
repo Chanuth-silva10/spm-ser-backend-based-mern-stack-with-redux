5eed2925fde3831849d142d3770f33248af4a6d2
ImashaKuruppu25 <binaryDemonss@gmail.com>
2022-09-04 15:54:04 +0530

    initial commit from order feature

--- backend/app.js
+++ backend/app.js
@@ -8,24 +8,19 @@ const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(fileUpload({useTempFiles: true}));
-
-
+app.use(fileUpload({ useTempFiles: true }));
 
 // Route imports
 const product = require("./routes/ProductRoute");
 const user = require("./routes/UserRoute");
 
+app.use("/api/v2", product);
 
-app.use("/api/v2",product);
-
-app.use("/api/v2",user);
-
-
+app.use("/api/v2", user);
 
 // it's for errorHandeling
 app.use(ErrorHandler);
 
-module.exports = app
+module.exports = app;
