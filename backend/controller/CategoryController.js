48ae8d1d94b442b827b5f7e88b24e9a47a1ffb20
shalinid99 <87404994+shalinid99@users.noreply.github.com>
2022-09-06 23:51:33 +0530

    Category update api implemented

--- backend/controller/CategoryController.js
+++ backend/controller/CategoryController.js
@@ -26,7 +26,33 @@ exports.addCatergory = catchAsyncErrors(async (req, res) => {
 });
 
 //get category
-exports.getCatergory = catchAsyncErrors(async(req,res) => {
-  const catergory = await Catergory.find()
-  res.status(200).json({ success:true, catergory});
-})
+exports.getCatergory = catchAsyncErrors(async (req, res) => {
+  const catergory = await Catergory.find();
+  res.status(200).json({ success: true, catergory });
+});
+
+//update category
+
+exports.updateCatagory = catchAsyncErrors(async (req, res, next) => {
+  const {
+    catergoryName,
+    catergoryDescripition,
+    catergoryImage,
+    productsInCatergory,
+  } = req.body;
+
+  const updatedCategory = await Catergory.findByIdAndUpdate(req.params.id);
+
+  if (!updatedCategory) {
+    return next(new ErrorHandler("No category found with this id", 404));
+  }
+
+  await updatedCategory.updateOne({
+    catergoryName,
+    catergoryDescripition,
+    catergoryImage,
+    productsInCatergory,
+  });
+
+  res.status(200).json({ success: true, updatedCategory });
+});
--- backend/routes/CategoryRoute.js
+++ backend/routes/CategoryRoute.js
@@ -4,11 +4,13 @@ const express = require("express");
 const {
   addCatergory,
   getCatergory,
+  updateCatagory,
 } = require("../controller/CategoryController");
 
 const router = express.Router();
 
 router.route("/catergory/addCatergory").post(addCatergory); //assingning the file imported -addcatergory
 router.route("/catergory/getCatergory").get(getCatergory);
+router.route("/catergory/updateCatergory/:id").put(updateCatagory);
 
 module.exports = router;
