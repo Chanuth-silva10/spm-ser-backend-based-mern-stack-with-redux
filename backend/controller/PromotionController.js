f0e7b2e1d433946db332f5b4eda016249deac5af
Ihill Dewpura <88815545+Sandushan8@users.noreply.github.com>
2022-09-06 23:18:53 +0530

    Full Crud with Report filter

--- backend/controller/PromotionController.js
+++ backend/controller/PromotionController.js
@@ -1,10 +1,19 @@
 const Promotion = require('../models/PromotionModel.js')
 
 exports.createpromo = async(req,res)=>{
+    let validation = validateinput(req.body)
+    if(validation){
+        return res.send({message:validation})
+    }
+    let on = req.body.othernotes
+    if(!req.body.othernotes){
+        on = 'none'
+    }
+
     const newpromotion = new Promotion({
         ID:req.body.promoid,
         Name:req.body.name,
-        OtherNotes:req.body.othernotes,
+        OtherNotes:on,
         Type:req.body.type,
         Discount:req.body.discount,
         Conditions:req.body.conditions
@@ -19,6 +28,13 @@ exports.createpromo = async(req,res)=>{
 exports.updatepromo = async(req,res)=>{
     let id = req.params.id
     await Promotion.findByIdAndUpdate(id,req.body)
+    .then(res.send({message:'Success'}))
+}
+
+exports.updatepromostatus = async(req,res) =>{
+    let id = req.params.id
+    let status = req.params.status
+    await Promotion.updateOne({_id:id},{$set:{Status:status}})
     .then(res.send({message:'Success'}))
 }
 
@@ -39,15 +55,60 @@ exports.findonepromo = async(req,res)=>{
 
 }
 
-exports.filterpromo = async(req,res)=>{
-
+exports.filterpromobystatus = async(req,res)=>{
+    let status = req.params.status
+    await Promotion.find({Status:status}).then(data=>{
+    res.send(data)})
+}
+exports.filterpromobytype = async(req,res)=>{
+    let type = req.params.type
+    await Promotion.find({Type:type}).then(data=>{
+    res.send(data)})
+}
+exports.filterpromobyboth = async(req,res)=>{
+    let status = req.params.status
+    let type = req.params.type
+    await Promotion.find({Status:status,Type:type}).then(data=>{
+    res.send(data)})
 }
 
 function validateinput(values){
+    const IDpattern = /^[a-zA-Z0-9]{0,6}$/
+    const Namepattern = /^[a-zA-Z\s]{0,20}$/
+    const Discountpattern = /^[0-9]+%{0,3}$/
+    const Conditionspattern = /^[a-zA-Z0-9\s]+$/
+
     let ID = values.promoid
     let Name = values.name
-    let OtherNotes = values.othernotes
     let Type = values.type
     let Discount = values.discount
     let Conditions = values.conditions
+
+    if(!ID){
+        return 'ID value can not be null'
+    }
+    if(!Name){
+        return 'Name value can not be null'
+    }
+    if(!Type){
+        return 'Type value can not be null'
+    }
+    if(!Discount){
+        return 'Discount value can not be null'
+    }
+    if(!Conditions){
+        return 'Conditions value can not be null'
+    }
+    if(!IDpattern.test(ID)){
+        return 'ID can only contain letters and numbers up to 6 characters'
+    }
+    if(!Namepattern.test(Name)){
+        return 'Name can only contain letters upto 20 characters'
+    }
+    if(!Discountpattern.test(Discount)){
+        return 'Discount can only contain valid percentage value'
+    }
+    if(!Conditionspattern.test(Conditions)){
+        return 'Conditions can only contain letters and numbers'
+    }
 }
--- backend/routes/PromotionRoute.js
+++ backend/routes/PromotionRoute.js
@@ -3,13 +3,20 @@ const{
     createpromo,
     findallpromo,
     updatepromo,
-    deletepromo
+    updatepromostatus,
+    deletepromo,
+    filterpromobystatus,
+    filterpromobytype,
+    filterpromobyboth
 } = require('../controller/PromotionController')
 
 
 router.route('/promotion').get(findallpromo)
 router.route('/promotion').post(createpromo)
 router.route('/promotion/:id').put(updatepromo)
+router.route('/promotion/:id/:status').patch(updatepromostatus)
 router.route('/promotion/:id').delete(deletepromo)
-
+router.route('/promotion/report/bystatus/:status').get(filterpromobystatus)
+router.route('/promotion/report/bytype/:type').get(filterpromobytype)
+router.route('/promotion/report/byboth/:status/:type').get(filterpromobyboth)
 module.exports = router
