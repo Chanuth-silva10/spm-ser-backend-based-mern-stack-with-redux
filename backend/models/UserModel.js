c81866863ccdc1c8b1d1eeb48a5d04edc59ad019
Chanuth10 <it20074418@my.sliit.lk>
2022-09-13 13:46:58 +0530

    Change password length

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, "Please enter your password!"],
-    minlength: [2, "Password should be greater than 8 characters"],
+    minlength: [8, "Password should be greater than 8 characters"],
     select: false,
   },
   avatar: {
